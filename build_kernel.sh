aa2cc5abc805fb3c571faa1bb996e31ac0e0f57f
nolan-archie <home.nolanarchie@gmail.com>
2025-04-07 20:43:17 +0000

    use python2 to run scripts/gen_build_config.py
    fixes: SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
    Signed-off-by: Ravindu Deshan <Droidcasts@protonmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+sudo apt update && sudo apt install python2
+
 cd kernel-5.10
-python scripts/gen_build_config.py --kernel-defconfig a16_00_defconfig --kernel-defconfig-overlays entry_level.config -m user -o ../out/target/product/a16/obj/KERNEL_OBJ/build.config
+python2 scripts/gen_build_config.py --kernel-defconfig a16_00_defconfig --kernel-defconfig-overlays entry_level.config -m user -o ../out/target/product/a16/obj/KERNEL_OBJ/build.config
 
 export ARCH=arm64
 export PLATFORM_VERSION=13
