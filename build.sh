3af2d28d4f8c6b4131a17cf3963655dcf3a77020
Ravindu Deshan <droidcasts@protonmail.com>
2025-04-08 08:25:41 +0530

    build script: Install the requirements for building the kernel when running the script for the first time
    In our case, only python2 and rsync is required
    
    Signed-off-by: Ravindu Deshan <droidcasts@protonmail.com>

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+
+echo -e "\n[INFO]: BUILD STARTED..!\n"
+
+export WDIR="$(pwd)"
+mkdir -p "${WDIR}/dist"
+
+# Init submodules
+git submodule init && git submodule update
+
+# Install the requirements for building the kernel when running the script for the first time
+if [ ! -f ".requirements" ]; then
+    echo -e "\n[INFO]: INSTALLING REQUIREMENTS..!\n"
+    {
+        sudo apt update
+        sudo apt install -y rsync python2
+    } && touch .requirements
+fi
