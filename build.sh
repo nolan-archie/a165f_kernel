64d1ecaa16923050ca4a23df0676513141c2f1e9
ravindu644 <droidcasts@protonmail.com>
2025-12-31 09:11:44 +0530

    updated build script to build vendor_boot and vendor_dlkm
    Signed-off-by: ravindu644 <droidcasts@protonmail.com>

--- build.sh
+++ build.sh
@@ -3,38 +3,40 @@ set -x
 
 echo -e "\n[INFO]: BUILD STARTED..!\n"
 
-export WDIR="$(pwd)"
-mkdir -p "${WDIR}/dist"
+export SCRIPT_DIR="$(dirname $(readlink -fq $0))"
+mkdir -p "${SCRIPT_DIR}/dist"
 
 # Init submodules
 git submodule init && git submodule update
 
 # Install the requirements for building the kernel when running the script for the first time
-if [ ! -f ".requirements" ] && [ "$USER" != "ravindu644" ]; then
-    echo -e "\n[INFO]: INSTALLING REQUIREMENTS..!\n"
-    {
-        sudo apt update
-        sudo apt install -y rsync python2
-    } && touch .requirements
-fi
+check_requirements_and_install_tc(){
+    if [ ! -f ".requirements" ] && [ "$USER" != "ravindu644" ]; then
+        echo -e "\n[INFO]: INSTALLING REQUIREMENTS..!\n"
+        {
+            sudo apt update
+            sudo apt install -y rsync python2
+        } && touch .requirements
+    fi
 
-# Init Samsung's ndk
-if [[ ! -d "${WDIR}/kernel/prebuilts" || ! -d "${WDIR}/prebuilts" ]]; then
-    echo -e "\n[INFO] Cloning Samsung's NDK...\n"
-    curl -LO "https://github.com/ravindu644/android_kernel_a165f/releases/download/toolchain/toolchain.tar.gz"
-    tar -xf toolchain.tar.gz && rm toolchain.tar.gz
-    cd "${WDIR}"
-fi
+    # Init Samsung's ndk
+    if [[ ! -d "${SCRIPT_DIR}/kernel/prebuilts" || ! -d "${SCRIPT_DIR}/prebuilts" ]]; then
+        echo -e "\n[INFO] Cloning Samsung's NDK...\n"
+        curl -LO "https://github.com/ravindu644/android_kernel_a165f/releases/download/toolchain/toolchain.tar.gz"
+        tar -xf toolchain.tar.gz && rm toolchain.tar.gz
+        cd "${SCRIPT_DIR}"
+    fi
+}
 
 # Localversion
 if [ -z "$BUILD_KERNEL_VERSION" ]; then
     export BUILD_KERNEL_VERSION="dev"
 fi
 
-echo -e "CONFIG_LOCALVERSION_AUTO=n\nCONFIG_LOCALVERSION=\"-ravindu644-${BUILD_KERNEL_VERSION}\"\n" > "${WDIR}/custom_defconfigs/version_defconfig"
+echo -e "CONFIG_LOCALVERSION_AUTO=n\nCONFIG_LOCALVERSION=\"-ravindu644-${BUILD_KERNEL_VERSION}\"\n" > "${SCRIPT_DIR}/custom_defconfigs/version_defconfig"
 
 # CHANGED DIR
-cd "${WDIR}/kernel-5.10"
+cd "${SCRIPT_DIR}/kernel-5.10"
 
 # Cook the build config
 python2 scripts/gen_build_config.py \
@@ -51,7 +53,7 @@ export CROSS_COMPILE_COMPAT="arm-linux-gnueabi-"
 export OUT_DIR="../out/target/product/a16/obj/KERNEL_OBJ"
 export DIST_DIR="../out/target/product/a16/obj/KERNEL_OBJ"
 export BUILD_CONFIG="../out/target/product/a16/obj/KERNEL_OBJ/build.config"
-export MERGE_CONFIG="${WDIR}/kernel-5.10/scripts/kconfig/merge_config.sh"
+export MERGE_CONFIG="${SCRIPT_DIR}/kernel-5.10/scripts/kconfig/merge_config.sh"
 
 # Build options
 export GKI_KERNEL_BUILD_OPTIONS="
@@ -59,16 +61,16 @@ export GKI_KERNEL_BUILD_OPTIONS="
     KMI_SYMBOL_LIST_STRICT_MODE=0 \
     ABI_DEFINITION= \
     BUILD_BOOT_IMG=1 \
-    MKBOOTIMG_PATH=${WDIR}/mkbootimg/mkbootimg.py \
+    MKBOOTIMG_PATH=${SCRIPT_DIR}/mkbootimg/mkbootimg.py \
     KERNEL_BINARY=Image.gz \
     BOOT_IMAGE_HEADER_VERSION=4 \
     SKIP_VENDOR_BOOT=1 \
     AVB_SIGN_BOOT_IMG=1 \
     AVB_BOOT_PARTITION_SIZE=67108864 \
-    AVB_BOOT_KEY=${WDIR}/mkbootimg/tests/data/testkey_rsa2048.pem \
+    AVB_BOOT_KEY=${SCRIPT_DIR}/mkbootimg/tests/data/testkey_rsa2048.pem \
     AVB_BOOT_ALGORITHM=SHA256_RSA2048 \
     AVB_BOOT_PARTITION_NAME=boot \
-    GKI_RAMDISK_PREBUILT_BINARY=${WDIR}/oem_prebuilt_images/gki-ramdisk.lz4 \
+    GKI_RAMDISK_PREBUILT_BINARY=${SCRIPT_DIR}/oem_prebuilt_images/gki-ramdisk.lz4 \
     LTO=full \
 "
 
@@ -78,7 +80,7 @@ export MKBOOTIMG_EXTRA_ARGS="
     --os_patch_level 2025-05-00 \
     --pagesize 4096 \
 "
-export GKI_RAMDISK_PREBUILT_BINARY="${WDIR}/oem_prebuilt_images/gki-ramdisk.lz4"
+export GKI_RAMDISK_PREBUILT_BINARY="${SCRIPT_DIR}/oem_prebuilt_images/gki-ramdisk.lz4"
 
 # Run menuconfig only if you want to.
 # It's better to use MAKE_MENUCONFIG=0 when everything is already properly enabled, disabled, or configured.
@@ -89,23 +91,38 @@ if [ "$MAKE_MENUCONFIG" = "1" ]; then
 fi
 
 # CHANGED DIR
-cd "${WDIR}/kernel"
+cd "${SCRIPT_DIR}/kernel"
 
 # Main cooking progress
 build_kernel(){
     ( env ${GKI_KERNEL_BUILD_OPTIONS} ./build/build.sh || exit 1 ) && \
-        ( cp "${WDIR}/out/target/product/a16/obj/KERNEL_OBJ/boot.img" "${WDIR}/dist" 
-        cp "${WDIR}/out/target/product/a16/obj/KERNEL_OBJ/kernel-5.10/arch/arm64/boot/Image.gz" "${WDIR}/dist" )
+        ( cp "${SCRIPT_DIR}/out/target/product/a16/obj/KERNEL_OBJ/boot.img" "${SCRIPT_DIR}/dist" 
+        cp "${SCRIPT_DIR}/out/target/product/a16/obj/KERNEL_OBJ/kernel-5.10/arch/arm64/boot/Image.gz" "${SCRIPT_DIR}/dist" )
+}
+
+# build vendor boot
+build_vendor_boot(){
+    SCRIPT_DIR="${SCRIPT_DIR}" \
+        "${SCRIPT_DIR}/prebuilts_helio_g99/scripts/build_vendor_boot.sh"
+}
+
+# build vendor dlkm
+build_vendor_dlkm(){
+    SCRIPT_DIR="${SCRIPT_DIR}" \
+        "${SCRIPT_DIR}/prebuilts_helio_g99/scripts/build_vendor_dlkm.sh"
 }
 
 build_tar(){
     echo -e "\n[INFO] Creating an Odin flashable tar..\n"
 
-    cd "${WDIR}/dist"
-    tar -cvf "KernelSU-Next-SM-A165F-${BUILD_KERNEL_VERSION}.tar" boot.img && rm boot.img
+    cd "${SCRIPT_DIR}/dist"
+    tar -cvf "KernelSU-Next-SM-A165F-${BUILD_KERNEL_VERSION}.tar" boot.img vendor_boot.img && rm boot.img vendor_boot.img
     echo -e "\n[INFO] Build Finished..!\n"
-    cd "${WDIR}"
+    cd "${SCRIPT_DIR}"
 }
 
+check_requirements_and_install_tc
 build_kernel || exit 1
 build_tar
+build_vendor_boot
+build_vendor_dlkm
